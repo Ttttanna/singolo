9ad2c51eaae20f04a816a0ee129c03fb249ed323
Tatsiana Alsheuskaya <ttttanna@gmail.com>
2020-03-29 19:13:36 +0300

    feat: add responsive

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-//header navigation
-/*const NAVIGATION = document.querySelector(".navigation");
-NAVIGATION.addEventListener("click", (event) => {
-    if(event.target.tagName == "A") {
-      NAVIGATION.querySelectorAll("A").forEach(el => el.classList.remove("chosen"));
-      event.target.classList.add("chosen");
-    }
- })*/
-
  document.addEventListener("scroll", onScroll);
  function onScroll(event) {
      const currentPos = window.scrollY;
      const scrollPoints = document.querySelectorAll(".to-scroll");
      const NAVIGATION = document.querySelector(".navigation");
      const links = NAVIGATION.querySelectorAll("a");
+     let headerHeight = document.querySelector(".header-container").offsetHeight + 1;
 
      scrollPoints.forEach((el) => {
          el.getAttribute("id");
-         if (el.offsetTop <= currentPos && (el.offsetTop + el.offsetHeight) > currentPos) {
-             //console.log(el.offsetTop);
-             //console.log(currentPos);
-
-             links.forEach((a) => {
+         if (currentPos === 0) {
+             links.forEach((a)=> {
+                 a.classList.remove("chosen");
+                 links[0].classList.add("chosen");
+             })
+         }
+         if ((el.offsetTop - headerHeight) < currentPos && (el.offsetTop + el.offsetHeight - headerHeight) > currentPos) {
+               links.forEach((a) => {
                  a.classList.remove("chosen");
                  if (el.getAttribute("id") === a.getAttribute("href").substring(1)) {
                      a.classList.add ("chosen");
@@ -85,40 +80,6 @@ NEXT.addEventListener("click", function() {
     }
 });
 
-/*const PREV = document.querySelector('.arrow-left');
-const NEXT = document.querySelector('.arrow-right');
-
-let items = document.querySelectorAll('.item');
-
-let i = 0;
-
-
-PREV.onclick = function() {
-    items[i].classList.remove("active");
-    i--;
-
-    if (i < 0) {
-        i = items.length - 1;
-    }
-
-    items[i].classList.add("active");
-}
-
-NEXT.onclick = function() {
-    items[i].classList.remove("active");
-    i++;
-
-    if (i >= items.length) {
-        i = 0;
-    }
-
-    items[i].classList.add("active");
-}*/
-
-
-
-
-
 //Dark screen 
 let phone = document.querySelector(".vhome-button");
 let screen = document.querySelector(".off");
@@ -182,8 +143,8 @@ let messageDescription = document.querySelector(".message-description");
 let body = document.querySelector("body");
 
 description.onkeyup  = function(event) {
-    if (event.target.value.length > 100) {
-        event.target.value = event.target.value.substring(0, 100);
+    if (event.target.value.length > 500) {
+        event.target.value = event.target.value.substring(0, 500);
     }
 }
 
@@ -202,5 +163,28 @@ form.onsubmit = function(event) {
         userEmail.value = '';
     }
 }
+
+//Burger menu activation
+let burgerMenu = document.querySelector(".burger");
+let menuLines = document.querySelector(".burger__line");
+let nav = document.querySelector(".header__navigation");
+let logo = document.querySelector(".logo");
+
+burgerMenu.onclick = function() {
+    menuLines.classList.toggle("open");
+    nav.classList.toggle("activation");
+    logo.classList.toggle("bur");
+    body.classList.toggle("lock");
+}
+
+
+const NAVIGATION = document.querySelector(".navigation");
+NAVIGATION.addEventListener("click", (event) => {
+    if(event.target.tagName == "A") {
+      menuLines.classList.remove("open");
+      nav.classList.remove("activation");
+      body.classList.remove("lock");
+    }
+ })
 
 
